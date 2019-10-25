6164f99d6156a4b5ed027e415f681d804cf7362b
Hannah Olukoye <hannaholukoye@gmail.com>
2019-10-25 23:10:04 +0300

    Feat: Adding a navigation drawer in the trivia app.

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,28 +19,40 @@ package com.example.android.navigation
 import androidx.databinding.DataBindingUtil
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    // TODO (05) Add private lateinit vars drawerLayout and appBarConfiguration
+    // DONE (05) Add private lateinit vars drawerLayout and appBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        // TODO (06) Initialize drawerLayout var from binding
+        // DONE (06) Initialize drawerLayout var from binding
+        drawerLayout = binding.drawerLayout
 
         val navController = this.findNavController(R.id.myNavHostFragment)
-        // TODO (07) Add the DrawerLayout as the second parameter to setupActionBarWithNavController
-        NavigationUI.setupActionBarWithNavController(this, navController)
-        // TODO (08) Create appBarConfiguration with the navController.graph and drawerLayout
-        // TODO (09) Hook the navigation UI up to the navigation view. (navView)
+        // DONE (07) Add the DrawerLayout as the second parameter to setupActionBarWithNavController
+
+        // DONE (08) Create appBarConfiguration with the navController.graph and drawerLayout
+
+        // DONE (09) Hook the navigation UI up to the navigation view. (navView)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        // TODO (10) Replace navController.navigateUp with NavigationUI.navigateUp with drawerLayout param
-        return navController.navigateUp()
+        // DONE (10) Replace navController.navigateUp with NavigationUI.navigateUp with drawerLayout param
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+
+
     }
 }
