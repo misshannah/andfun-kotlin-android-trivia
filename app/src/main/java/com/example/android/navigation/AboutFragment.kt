04a89e2a405f791f7c94c7f0320901eae7cfdabe
Hannah Olukoye <hannaholukoye@gmail.com>
2019-10-25 17:37:22 +0300

    Feat: Adding a menu on the trivia app.

--- app/src/main/java/com/example/android/navigation/AboutFragment.kt
+++ app/src/main/java/com/example/android/navigation/AboutFragment.kt
@@ -17,10 +17,10 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class AboutFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
